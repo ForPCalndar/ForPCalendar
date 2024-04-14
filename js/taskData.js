f90d4957351e68057155b90bdf028cca88f0d71e
munjieun <mjsu10@naver.com>
2024-04-14 17:54:43 +0900

    rebase

---
+++ js/taskData.js
@@ -0,0 +1,53 @@
+const taskData = [
+  {
+    id: 1,
+    date: {
+      year: "2024",
+      month: "04",
+      day: "05",
+    },
+    todoList: [
+      {
+        time: "12:00",
+        title: "할 일1",
+        category: "일",
+      },
+    ],
+  },
+  {
+    id: 2,
+    date: {
+      year: "2024",
+      month: "04",
+      day: "10",
+    },
+    todoList: [
+      {
+        time: "18:00",
+        title: "할 일2",
+        category: "휴식",
+      },
+      {
+        time: "18:00",
+        title: "할 일2-2",
+        category: "일",
+      },
+    ],
+  },
+  {
+    id: 3,
+    date: {
+      year: "2024",
+      month: "04",
+      day: "08",
+    },
+    todoList: [
+      {
+        time: "14:00",
+        title: "할 일3",
+        category: "운동",
+      },
+    ],
+  },
+];
+export default taskData;
